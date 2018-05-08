7d2a3fa5eb4888f05c8bf7031e2fe832e4343d9d
Under <35243703+underdarkskies@users.noreply.github.com>
2018-05-08 13:35:39 -0400

    scanner upgrades

--- public/src/js/controllers/scanner.js
+++ public/src/js/controllers/scanner.js
@@ -26,8 +26,32 @@ angular.module('insight.system').controller('ScannerController',
       }
     };
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+    if (navigator.mediaDevices === undefined) {
+		navigator.mediaDevices = {};
+	}
+
+	// Some browsers partially implement mediaDevices. We can't just assign an object
+	// with getUserMedia as it would overwrite existing properties.
+	// Here, we will just add the getUserMedia property if it's missing.
+	if (navigator.mediaDevices.getUserMedia === undefined) {
+		navigator.mediaDevices.getUserMedia = function(constraints) {
+
+			// First get ahold of the legacy getUserMedia, if present
+			var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+			// Some browsers just don't implement it - return a rejected promise with an error
+			// to keep a consistent interface
+			if (!getUserMedia) {
+				return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+			}
+
+			// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+			return new Promise(function(resolve, reject) {
+				getUserMedia.call(navigator, constraints, resolve, reject);
+			});
+		}
+	}
+	// window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
     $scope.isMobile = isMobile.any();
     $scope.scannerLoading = false;
@@ -38,7 +62,7 @@ angular.module('insight.system').controller('ScannerController',
         canvas,
         $video,
         context,
-        localMediaStream;
+        stream;
 
     var _scan = function(evt) {
       if ($scope.isMobile) {
@@ -73,7 +97,7 @@ angular.module('insight.system').controller('ScannerController',
           reader.readAsDataURL(file);
         }
       } else {
-        if (localMediaStream) {
+        if (MediaStream.active) {
           context.drawImage(video, 0, 0, 300, 225);
 
           try {
@@ -88,31 +112,43 @@ angular.module('insight.system').controller('ScannerController',
     };
 
     var _successCallback = function(stream) {
-      video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-      localMediaStream = stream;
+      var video = document.getElementById('qrcode-scanner-video');
+      var $video = angular.element(video);
+	  video.srcObject = stream;
       video.play();
-      setTimeout(_scan, 1000);
+	  setTimeout(_scan, 1000);
     };
 
     var _scanStop = function() {
       $scope.scannerLoading = false;
       $modalInstance.close();
       if (!$scope.isMobile) {
-        if (localMediaStream.stop) localMediaStream.stop();
-        localMediaStream = null;
-        video.src = '';
+		var video = document.getElementById('qrcode-scanner-video');
+		var qrstream = video.srcObject;
+		var tracks = qrstream.getTracks();
+		tracks.forEach(function(track) {
+		  track.stop();
+		});
+        video.srcObject = null;
       }
     };
 
     var _videoError = function(err) {
       console.log('Video Error: ' + JSON.stringify(err));
+	  errorMsg('getUserMedia error: ' + error.name, error);
       _scanStop();
     };
 
-    qrcode.callback = function(data) {
+	function errorMsg(msg, error) {
+	  if (typeof error !== 'undefined') {
+		console.error(error);
+	  }
+	}
+
+	qrcode.callback = function(data) {
       _scanStop();
 
-      var str = (data.indexOf('ravencoin:') === 0) ? data.substring(8) : data; 
+      var str = (data.indexOf('ravencoin:') === 0) ? data.substring(8) : data;
       console.log('QR code detected: ' + str);
       $searchInput
         .val(str)
@@ -136,13 +172,11 @@ angular.module('insight.system').controller('ScannerController',
           cameraInput = document.getElementById('qrcode-camera');
           cameraInput.addEventListener('change', _scan, false);
         } else {
-          video = document.getElementById('qrcode-scanner-video');
-          $video = angular.element(video);
           canvas.width = 300;
           canvas.height = 225;
           context.clearRect(0, 0, 300, 225);
 
-          navigator.getUserMedia({video: true}, _successCallback, _videoError);
+          navigator.mediaDevices.getUserMedia({video: true}).then(_successCallback).catch(_videoError);
         }
       }, 500);
     });
